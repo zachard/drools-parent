01a86b5e50efc4df01f683911df1ca0fcc7ad167
zachard <arieszpzeng@yeah.net>
2017-09-26 23:59:31 +0800

    finish: DRL文件中结果部分(RHS)实现Insert操作示例
    可参考的内容: DRL的RHS中实现Insert示例
                  KnowledgeBuilder实现DRL文件编译查看错误

---
+++ hello-drools/src/main/java/com/zachard/drools/hello/model/Customer.java
@@ -0,0 +1,51 @@
+/*
+ *  Copyright 2015-2017 zachard, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.zachard.drools.hello.model;
+
+/**
+ * DRL规则文件需要的实体类
+ * <pre>
+ * </pre>
+ *
+ * @author zachard
+ * @version 1.0.0
+ */
+public class Customer {
+
+	/**
+	 * 顾客姓名
+	 */
+	private String name;
+
+	/**
+	 * 获取顾客姓名
+	 * 
+	 * @return the name   顾客姓名
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * 设置顾客姓名
+	 * 
+	 * @param name 顾客姓名
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ hello-drools/src/main/java/com/zachard/drools/hello/rhs/InsertTest.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright 2015-2017 zachard, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.zachard.drools.hello.rhs;
+
+import java.util.Collection;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.definition.KnowledgePackage;
+import org.drools.io.ResourceFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 规则结果部分insert方法执行测试类
+ * <pre>
+ * </pre>
+ *
+ * @author zachard
+ * @version 1.0.0
+ */
+public class InsertTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(InsertTest.class);
+	
+	/**
+	 * 规则的结果集部分执行insert函数,将对象插入到Working Memory之中
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 1. 创建KnowledgeBuilder对象并获取KnowledgePackage对象
+		KnowledgeBuilder kBuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		kBuilder.add(ResourceFactory.newClassPathResource("rules/rhsInsert.drl", InsertTest.class), ResourceType.DRL);
+		
+		// 先判断DRL规则文件是否有错误
+		if (kBuilder.hasErrors()) {
+			KnowledgeBuilderErrors kBuilderErrors = kBuilder.getErrors();
+			kBuilderErrors.forEach(kBuilderError -> {
+				logger.info("规则文件中存在错误,错误信息如下: ");
+				logger.error(kBuilderError.getMessage());
+			});
+			
+			return;
+		}
+		
+		Collection<KnowledgePackage> kPackages = kBuilder.getKnowledgePackages();
+		
+		// 2. 创建KnowledgeBase对象并为其添加KnowledgePackage对象
+		KnowledgeBase kBase = KnowledgeBaseFactory.newKnowledgeBase();
+		kBase.addKnowledgePackages(kPackages);
+		
+		// 3. 创建StatefulKnowledgeSession对象,并为其传入属性,触发规则
+		StatefulKnowledgeSession statefulKnowledgeSession = kBase.newStatefulKnowledgeSession();
+		statefulKnowledgeSession.fireAllRules();
+		statefulKnowledgeSession.dispose();
+		
+		logger.info("End...");
+	}
+
+}
